fd8dfcef78048ffb26cf7c10639cc944a97c48ae
AhlaamK-tap <a.kazi@tap.company>
2020-05-13 16:53:40 +0300

    animate

--- app/src/main/java/company/tap/tapanimator_sample/MainActivity.kt
+++ app/src/main/java/company/tap/tapanimator_sample/MainActivity.kt
@@ -2,6 +2,7 @@ package company.tap.tapanimator_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import company.tap.tapanimationkit.AnimateView
 import company.tap.tapanimationkit.RenderAnimate
 import company.tap.tapanimationkit.viewsanim.Slide
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,9 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val render = RenderAnimate(this)
+
+        AnimateView.animateSlideRight(this)
+       /* val render = RenderAnimate(this)
         render.setAnimation(Slide().InUp(tv_hello))
         render.setDuration(5000)
-        render.start()
+        render.start()*/
     }
 }
